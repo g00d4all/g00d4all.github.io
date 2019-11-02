0b95b5a0ecb82170cb539a6eba7ab94d01439b22
g00d4all <55336244+g00d4all@users.noreply.github.com>
2019-11-02 11:02:59 -0400

    Update mq.js

--- math/mq.js
+++ math/mq.js
@@ -1,7 +1,7 @@
 var tikz=document.getElementsByClassName('tikz');
 for(var i = 0; i < tikz.length; i++) {
   tikz[i].src = "https://tex.s2cms.ru/svg/"+encodeURIComponent(tikz[i].alt.trim());
-  tikz[i].outerHTML = "<span style='display:block;text-align:center;'>"+tikz[i].outerHTML+"</span>";
+  tikz[i].outerHTML = "<span class='image block'>"+tikz[i].outerHTML+"</span>";
 }
 
 document.getElementById("collapse").onclick = function() {
