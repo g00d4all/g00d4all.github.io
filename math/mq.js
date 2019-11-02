75a9ead6987adaa732950455a46c8c1441c22bf4
g00d4all <55336244+g00d4all@users.noreply.github.com>
2019-11-01 22:03:21 -0400

    Update mq.js

--- math/mq.js
+++ math/mq.js
@@ -19,3 +19,35 @@ document.getElementById("collapse").onclick = function() {
     }
   this.value = (this.value == "➕" ? "➖" : "➕");
 };
+
+document.getElementById("print").onclick = function() {
+  var myWindow = window.open();
+  myWindow.document.write(`
+  <html>
+  <head>
+  <title>${document.getElementsByClassName("preview-paragraph-0 preview-line 0")[0].innerText.trim()}</title>
+  <style type="text/css">
+  html * {
+    font-family: Times, Times New Roman, serif;
+    }
+  li {
+    margin-bottom: 1em;
+    }
+  li br {
+    content: \"\A\" !important; display: block !important; margin-bottom: 0.25em !important;
+    }
+  </style>
+  </head>
+  <body>
+  </body>
+  </html>
+  `);
+  /*
+  myWindow.document.head.appendChild(document.getElementById("MJX-SVG-styles").cloneNode(true));
+  myWindow.document.head.appendChild(document.getElementById("Mathpix-styles").cloneNode(true));
+  */
+  myWindow.document.body.appendChild(document.getElementsByClassName("previewPage")[0].cloneNode(true));
+  myWindow.document.close();
+  myWindow.focus();
+  myWindow.print();
+}
