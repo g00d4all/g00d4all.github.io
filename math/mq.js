ad514dbf5dbc4c7712ecd7099491bf4934332524
g00d4all <55336244+g00d4all@users.noreply.github.com>
2019-11-01 23:10:39 -0400

    Update mq.js

--- math/mq.js
+++ math/mq.js
@@ -19,7 +19,7 @@ document.getElementById("print").onclick = function() {
   <head>
   <title>${document.getElementsByClassName("preview-paragraph-0 preview-line 0")[0].innerText.trim()}</title>
   <link rel="stylesheet" type="text/css" href="https://g00d4all.github.io/math/mq.css">
-  <script type="text/javascript" src="https://g00d4all.github.io/math/mq.js">
+  <script type="text/javascript" src="https://g00d4all.github.io/math/mq.js"></script>
   <style type="text/css">
   html * {
     font-family: Times, Times New Roman, serif;
