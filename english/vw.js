e68f97bee74037ca54282b553a0300fb495ba74a
g00d4all <55336244+g00d4all@users.noreply.github.com>
2019-11-02 21:56:54 -0400

    Update vw.js

--- english/vw.js
+++ english/vw.js
@@ -69,3 +69,41 @@ document.getElementById("print").onclick = function() {
   myWindow.document.close();
   myWindow.focus();
 }
+
+
+//set attributes of selected classes
+	var c_a = [
+    ['av', [
+		['onclick', 'var mp=document.getElementById(\'mp\'); mp.src=\'' + msroot + '\' + this.nextSibling.innerText; mp.load(); mp.play();']
+	]],
+	['bl', [
+		['type', 'text'],
+		['onblur', 'this.value=(this.value.toLowerCase().trim()==\'?\'|| this.value.toLowerCase().trim()==this.nextSibling.innerText.toLowerCase())?this.nextSibling.innerText:\'\';'],
+        ['onkeyup', 'if(event.keyCode==13){this.blur();}'],
+        ['ondblclick', 'this.value=this.nextSibling.innerText;']
+	]],
+	['cc', [
+		['onclick', 'this.style.border=\'1px solid blue\'']
+	]],
+	['ac', [
+		['onclick', 'if(this.hasChildNodes()){this.firstChild.click()}']
+	]],
+	['hint', [
+		['onclick', 'this.style.left=(this.style.left==\'-100px\'?0:\'-100px\'); document.getElementById(\'wdlist\').style.display=(document.getElementById(\'wdlist\').style.display==\'\'?\'none\':\'\')'],
+        ['onmouseover', 'this.style.left=0; document.getElementById(\'wdlist\').style.display=\'\''],
+		['onmouseout', 'this.style.left= \'-100px\'; document.getElementById(\'wdlist\').style.display=\'none\'']
+	]],
+	['mp', [
+		['onloadstart', 'this.style.display=\'\''],
+		['onended', 'this.style.display=\'none\'']
+	]]
+	];
+    
+	for (var i = 0; i < c_a.length; i++) {
+		var myEl = document.getElementsByClassName(c_a[i][0]);
+		for (var j = 0; j < myEl.length; j++) {
+			for (var k = 0; k < c_a[i][1].length; k++) {
+				myEl[j].setAttribute(c_a[i][1][k][0], c_a[i][1][k][1]);
+			}
+		}
+	}
