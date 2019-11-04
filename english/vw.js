463ba76b4b4a36c52de1d0321174c677775baad6
g00d4all <55336244+g00d4all@users.noreply.github.com>
2019-11-03 22:15:34 -0500

    Update vw.js

--- english/vw.js
+++ english/vw.js
@@ -69,10 +69,11 @@ document.getElementById("print").onclick = function() {
   <head>
   <title>${document.getElementsByClassName("preview-paragraph-0 preview-line 0")[0].innerText.trim()}</title>
   <link rel="stylesheet" type="text/css" href="https://g00d4all.github.io/english/vw.css">
+  <link rel="stylesheet" type="text/css" href="g00d4all.github.io/fonts/cm-unicode/Serif/cmu-serif.css">
   <script type="text/javascript" src="https://g00d4all.github.io/english/vw.js"></script>
   <style>
     html * { 
-    font-family: Times, "Times New Roman", Georgia, serif;
+    font-family: "CMU Serif", Times, "Times New Roman", Georgia, serif;
     }
     div {
     margin-top: 0;
