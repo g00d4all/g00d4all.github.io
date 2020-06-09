0774640ec229e6bd96cf617091aabf7a368dade3
g00d4all <55336244+g00d4all@users.noreply.github.com>
2020-06-09 00:44:58 -0400

    Update test.js

--- english/test.js
+++ english/test.js
@@ -36,7 +36,7 @@ document.getElementById("print").onclick = function() {
   </body>
   </html>
   `);
-  myWindow.document.body.appendChild(document.getElementsByClassName("previewPage")[0].cloneNode(true));
+  myWindow.document.body.appendChild(document.getElementsById("preview-content").cloneNode(true));
   myWindow.document.close();
   myWindow.focus();
   myWindow.print();
