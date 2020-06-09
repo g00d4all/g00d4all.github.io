ded6337cd135865b5680bf6820d8195f29565a01
g00d4all <55336244+g00d4all@users.noreply.github.com>
2020-06-09 00:22:03 -0400

    Update test.js

--- english/test.js
+++ english/test.js
@@ -1,1 +1,47 @@
 
+document.getElementById("collapse").onclick = function() {
+  var mySolution = document.getElementsByClassName("solution");
+  var myPreset = document.getElementsByClassName("preset");
+  if (this.value.charCodeAt(0).toString(16) == 2795) {
+    for(var i = 0; i < mySolution.length; i++) {
+      mySolution[i].style.display = "block";
+    }
+    for(var i = 0; i < myPreset.length; i++) {
+      myPreset[i].style.display = "none";
+    }
+    this.value = String.fromCharCode(0x2796);
+  }
+  else {
+    for(var i = 0; i < mySolution.length; i++) {
+      mySolution[i].style.display = "none";
+    }
+    for(var i = 0; i < myPreset.length; i++) {
+      myPreset[i].style.display = "";
+    }
+    this.value = String.fromCharCode(0x2795);
+  }
+}
+
+document.getElementById("print").onclick = function() {
+  var myWindow = window.open();
+  myWindow.document.write(`
+  <html>
+  <head>
+  <title>${document.getElementsByClassName("preview-paragraph-0 preview-line 0")[0].innerText.trim()}</title>
+  <link rel="stylesheet" type="text/css" href="https://g00d4all.github.io/english/test.css">
+  <script type="text/javascript" src="https://g00d4all.github.io/english/test.js"></script>
+  <style>
+  html * {
+    font-family: "CMU Serif", Times, "Times New Roman", serif;
+  }
+  </style>
+  </head>
+  <body>
+  </body>
+  </html>
+  `);
+  myWindow.document.body.appendChild(document.getElementsByClassName("previewPage")[0].cloneNode(true));
+  myWindow.document.close();
+  myWindow.focus();
+  myWindow.print();
+}
